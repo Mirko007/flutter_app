c8b50867cc542beee3930644c20dcbbddf5a46ca
Vanja Hršak <vanja.hrsak@billy.hr>
2019-12-05 14:12:50 +0100

    1.0.6+5 Push notification za IOS i android, IOS 10 problem riješen

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -2,13 +2,15 @@
 #include "GeneratedPluginRegistrant.h"
 
 @implementation AppDelegate
-if (@available(iOS 10.0, *)) {
-  [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
-}
-- (BOOL)application:(UIApplication *)application
+
+- (bool)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+  if (@available(iOS 10.0, *)) {
+      [UNUserNotificationCenter currentNotificationCenter].delegate =
+          (id<UNUserNotificationCenterDelegate>)self;
+    }
+    [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
